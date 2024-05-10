a8f0bb245e3dea178fa8ab313134e4459de2ea00
jmarcelotse <jmarcelotse@hotmail.com>
2024-05-10 13:44:46 -0300

    ajuste minikube drive

--- minikube/main.tf
+++ minikube/main.tf
@@ -1,6 +1,21 @@
 resource "null_resource" "install_minikube" {
   provisioner "local-exec" {
-    command = "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/"
+    command = "curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/ && minikube config set driver docker"
+    environment = {
+      MINIKUBE_WANT_REPORTERRORPROMPT = "false"
+    }
+  }
+
+  # Prevent Docker driver usage with root privileges
+  provisioner "local-exec" {
+    command     = "minikube status"
+    interpreter = ["bash", "-c"]
+    environment = {
+      MINIKUBE_WANT_REPORTERRORPROMPT = "false"
+    }
+
+    # Only execute if Minikube has been installed
+    when = "destroy"
   }
 }
 
@@ -8,6 +23,9 @@ resource "null_resource" "start_minikube" {
   depends_on = [null_resource.install_minikube]
 
   provisioner "local-exec" {
-    command = "minikube start"
+    command = "minikube start --force"
+    environment = {
+      MINIKUBE_WANT_REPORTERRORPROMPT = "false"
+    }
   }
 }
